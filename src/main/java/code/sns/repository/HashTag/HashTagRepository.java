61e836715e720cd7e0b93dde45c386a0264fbfe7
sseung12 <pon02129@naver.com>
2022-03-27 21:51:43 +0900

    [Add] hashTag 로직 작성중

--- src/main/java/code/sns/repository/HashTag/HashTagRepository.java
+++ src/main/java/code/sns/repository/HashTag/HashTagRepository.java
@@ -3,9 +3,11 @@ package code.sns.repository.HashTag;
 import code.sns.domain.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HashTagRepository extends JpaRepository<HashTag,Long> {
 
     Optional<HashTag> findByTagName(String hash);
+    List<HashTag> findByTagNameIn(List<String> tagNameList);
 }
